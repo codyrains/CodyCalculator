6e73fa8c07d3b8f130a3d6efde2925c8e6a4ce47
codyrains <cody.rains34@gmail.com>
2021-11-18 15:17:19 -0500

    fixing git

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stop = false;
+            while (!stop) 
+            {
+                Menu();
+            }
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Calculator Menu\n\n");
+            Console.WriteLine("1. Addition\n");
+            Console.WriteLine("2. Subtraction\n");
+            Console.WriteLine("3. Multiplication\n");
+            Console.WriteLine("4. Division\n");
+            Console.WriteLine("5. Quit\n");
+
+            Console.WriteLine("Enter your option.\n");
+            testc 
+        }
+
+    }
+}
