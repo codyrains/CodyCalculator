5154a0f9795d62c9f52ae77ecdaa79134dff2b04
codyrains <cody.rains34@gmail.com>
2021-11-18 16:08:15 -0500

    added some base code

---
+++ InputObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodyCalculator
+{
+    public class InputObject
+    {
+        public int valueOne { get; set; }
+        public int valueTwo { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,25 +10,80 @@ namespace CodyCalculator
     {
         static void Main(string[] args)
         {
-            var stop = false;
+            bool stop = false;
             while (!stop) 
             {
-                Menu();
+                int menuOption = Menu();
+
+                if(menuOption == 5)
+                {
+                    stop = true;
+                }
+                else
+                {
+                    MenuOptionSwitch(menuOption); 
+                }
             }
         }
 
-        static void Menu()
+        static int Menu()
         {
-            Console.WriteLine("Calculator Menu\n\n");
-            Console.WriteLine("1. Addition\n");
-            Console.WriteLine("2. Subtraction\n");
-            Console.WriteLine("3. Multiplication\n");
-            Console.WriteLine("4. Division\n");
+            Console.WriteLine("Calculator Menu\n");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
             Console.WriteLine("5. Quit\n");
-
-            Console.WriteLine("Enter your option.\n");
-            testc 
+            Console.WriteLine("Enter your option.");
+            string menuOption = Console.ReadLine();
+            return Int32.Parse(menuOption);
         }
 
+        static void MenuOptionSwitch(int menuOption)
+        {
+            switch(menuOption)
+            {
+                case 1:
+                    Addition();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+        static void Addition()
+        {
+
+            Console.Write("Enter first number: ");
+            Console.ReadLine();
+            Console.Write("Enter second number: ");
+            Console.ReadLine();
+        }
+
+        static void Subtraction()
+        {
+
+        }
+
+        static void Multiplication()
+        {
+
+        }
+
+        static void Division()
+        {
+
+        }
+
+        static InputObject GetInputs()
+        {
+            Console.Write("Enter first number: ");
+            Console.ReadLine();
+            Console.Write("Enter second number: ");
+            Console.ReadLine();
+        }
     }
 }
